93be43449f43c69b0151ecb7a469e6a15aa415b9
Timothy Ege <retibra@gmail.com>
2018-08-08 22:21:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Tim!"
+  end
+
+  get '/hometown' do
+  "My hometown is Houston!"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is Anthology by Thrice!"
+  end
+
 end
